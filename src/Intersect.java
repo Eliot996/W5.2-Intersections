187a3f9d8e2b92912bc0588980668aff3dd96ecd
Eliot996 <math10t6@stud.kea.dk>
2022-02-05 12:44:54 +0100

    added intersectOptimizedMaybe(), with print to see workings

--- src/Intersect.java
+++ src/Intersect.java
@@ -4,8 +4,8 @@ import java.util.List;
 public class Intersect {
 
     public static void main(String[] args) {
-        int[] intArr2 = {1, 4, 8, 9, 11, 15, 17, 28, 41, 59};
-        int[] intArr = {4, 7, 11, 17, 19, 20, 23, 28, 37, 59, 81};
+        int[] intArr = {1, 4, 8, 9, 11, 15, 17, 28, 41, 59};
+        int[] intArr2 = {4, 7, 11, 17, 19, 20, 23, 28, 37, 59, 81};
 
         ArrayList<Integer> arrayList1 = new ArrayList<>();
         ArrayList<Integer> arrayList2 = new ArrayList<>();
@@ -18,7 +18,7 @@ public class Intersect {
             arrayList2.add(i);
         }
 
-        System.out.println(intersect(arrayList1, arrayList2));
+        System.out.println(intersectOptimizedMaybe(arrayList1, arrayList2));
 
     }
 
@@ -36,6 +36,31 @@ public class Intersect {
         return intersections;
     }
 
+    public static List<Integer> intersectOptimizedMaybe(List<Integer> list1, List<Integer> list2) {
+        ArrayList<Integer> intersections = new ArrayList<>();
 
+        int currentPosition = 0;
+
+        for (int i : list1)
+        {
+            System.out.println("looking at: " + i);
+            while (currentPosition < list2.size() && i >= list2.get(currentPosition))
+            {
+                System.out.println("In list2 at position " + currentPosition + ": " + list2.get(currentPosition));
+                if (i == list2.get(currentPosition))
+                {
+                    System.out.println("Target found");
+                    if (!intersections.contains(i))
+                    {
+                        System.out.println("Added " + i + " to intersections");
+                        intersections.add(i);
+                    }
+                }
+                currentPosition++;
+            }
+        }
+
+        return intersections;
+    }
 
 }
