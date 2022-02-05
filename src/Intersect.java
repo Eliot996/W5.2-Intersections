392b61ee16db15818f92dab8d703f7a92c45b618
Eliot996 <math10t6@stud.kea.dk>
2022-02-05 12:58:53 +0100

    added comments, kept the prints to console in intersectOptimizedMaybe()

--- src/Intersect.java
+++ src/Intersect.java
@@ -25,9 +25,13 @@ public class Intersect {
     public static List<Integer> intersect(List<Integer> list1, List<Integer> list2) {
         ArrayList<Integer> intersections = new ArrayList<>();
 
+        // take int from list1
         for (int i : list1) {
+            // check if it is present in list2
             if (list2.contains(i)) {
+                // check if it is not present in intersections
                 if (!intersections.contains(i)) {
+                    // then add the int to intersections
                     intersections.add(i);
                 }
             }
@@ -39,23 +43,34 @@ public class Intersect {
     public static List<Integer> intersectOptimizedMaybe(List<Integer> list1, List<Integer> list2) {
         ArrayList<Integer> intersections = new ArrayList<>();
 
+        // set the current position to 0
         int currentPosition = 0;
 
+        // take int from list1
         for (int i : list1)
         {
-            System.out.println("looking at: " + i);
+            System.out.println("looking at: " + i); // debugging
+
+            // if the currentPosition smaller than the size of list2
+            // and while the taken int is larger than or equal to the int at the currentPosition in list2
             while (currentPosition < list2.size() && i >= list2.get(currentPosition))
             {
-                System.out.println("In list2 at position " + currentPosition + ": " + list2.get(currentPosition));
+                System.out.println("In list2 at position " + currentPosition + ": " + list2.get(currentPosition)); // debugging
+
+                // if the two ints are equal
                 if (i == list2.get(currentPosition))
                 {
-                    System.out.println("Target found");
-                    if (!intersections.contains(i))
+                    System.out.println("Target found"); // debugging
+
+                    // if i does not equal the last int in intersections then add i to intersections
+                    if (i != intersections.get(intersections.size() - 1))
                     {
-                        System.out.println("Added " + i + " to intersections");
+                        System.out.println("Added " + i + " to intersections"); // debugging
                         intersections.add(i);
                     }
                 }
+
+                // increment the currentPosition
                 currentPosition++;
             }
         }
