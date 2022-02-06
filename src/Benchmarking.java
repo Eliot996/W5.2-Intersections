75dcc1e65887ba536139bedac2f9f301243c144f
Eliot996 <math10t6@stud.kea.dk>
2022-02-06 17:51:52 +0100

    added constant to dictate amount of benchmarking runs, and fixed naming

--- src/Benchmarking.java
+++ src/Benchmarking.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Random;
 
 public class Benchmarking {
-    private static final int arraySizeMinimum = 8000;
-    private static final int arraySizeVariance = 4000;
+    private static final int ARRAY_SIZE_MINIMUM = 8000;
+    private static final int ARRAY_SIZE_VARIANCE = 4000;
+    private static final int BENCHMARKING_RUNS= 100;
+
     
     private static final Random random = new Random();
     private static final ArrayList<Long> timesOptimized = new ArrayList<>();
@@ -14,7 +16,7 @@ public class Benchmarking {
 
     public static void main(String[] args) {
         System.out.print("Working");
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < BENCHMARKING_RUNS; i++) {
             runBenchmark();
             System.out.print(".");
         }
@@ -26,7 +28,7 @@ public class Benchmarking {
 
         System.out.println("It took on average " + averageForIntersect + " microseconds for the regular intersect");
         System.out.println("It took on average " + averageForOptimized + " microseconds for the optimized intersect");
-        System.out.println("Did the two algorithms get the same answer everytime? " + sameResult);
+        System.out.println("Did the two algorithms get the same answer everytime? " + (sameResult? "Yes" : "No"));
         System.out.println();
         System.out.println("The optimized algorithm was " + averageForIntersect/averageForOptimized + " times faster!");
     }
@@ -39,16 +41,14 @@ public class Benchmarking {
         long startTime = System.nanoTime();
         List<Integer> listForIntersect = Intersect.intersect(arrayList1, arrayList2);
         long endTime = System.nanoTime();
-
-        // saves time to complete
+        // saves time to complete in microseconds
         timesIntersect.add((endTime - startTime)/1000);
 
         // runs the test for intersectOptimizedMaybe()
         startTime = System.nanoTime();
         List<Integer> listForOptimized = Intersect.intersectOptimizedMaybe(arrayList1, arrayList2);
         endTime = System.nanoTime();
-
-        // saves time to complete
+        // saves time to complete in microseconds
         timesOptimized.add((endTime - startTime)/1000);
 
         // check that they have the same answer
@@ -68,7 +68,7 @@ public class Benchmarking {
 
     private static ArrayList<Integer> randomizeData() {
         ArrayList<Integer> arrayList = new ArrayList<>();
-        int amountOfTimes = arraySizeMinimum + random.nextInt(arraySizeVariance);
+        int amountOfTimes = ARRAY_SIZE_MINIMUM + random.nextInt(ARRAY_SIZE_VARIANCE);
 
         for (int i = 0; i < amountOfTimes; i++) {
             arrayList.add(random.nextInt(1000000));
